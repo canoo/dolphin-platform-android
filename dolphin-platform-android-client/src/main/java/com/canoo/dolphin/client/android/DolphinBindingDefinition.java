cb8de2f5be50c5f7580f676a873122339bd63f24
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-01-03 08:46:15 +0100

    Additional bindings

--- dolphin-platform-android-client/src/main/java/com/canoo/dolphin/client/android/DolphinBindingDefinition.java
+++ dolphin-platform-android-client/src/main/java/com/canoo/dolphin/client/android/DolphinBindingDefinition.java
@@ -3,6 +3,9 @@ package com.canoo.dolphin.client.android;
 import android.databinding.BindingAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.canoo.dolphin.event.ValueChangeEvent;
 import com.canoo.dolphin.event.ValueChangeListener;
@@ -13,7 +16,69 @@ public class DolphinBindingDefinition {
     private DolphinBindingDefinition() {
     }
 
-    @BindingAdapter({"app:binding"})
+    @BindingAdapter({"app:progressBinding"})
+    public static void bindViewEnabledToBooleanProperty(final ProgressBar view, final Property<Integer> property) {
+
+        property.onChanged(new ValueChangeListener<Integer>() {
+            @Override
+            public void valueChanged(ValueChangeEvent<? extends Integer> evt) {
+                int newValue = evt.getNewValue() == null ? 0 : evt.getNewValue();
+                if(newValue != view.getProgress()) {
+                    view.setProgress(newValue);
+                }
+            }
+        });
+        int newValue = property.get() == null ? 0 : property.get();
+        if(newValue != view.getProgress()) {
+            view.setProgress(newValue);
+        }
+    }
+
+    @BindingAdapter({"app:enabledBinding"})
+    public static void bindViewEnabledToBooleanProperty(final View view, final Property<Boolean> property) {
+
+        property.onChanged(new ValueChangeListener<Boolean>() {
+            @Override
+            public void valueChanged(ValueChangeEvent<? extends Boolean> evt) {
+                boolean newValue = evt.getNewValue() == null ? false : evt.getNewValue();
+                if(newValue != view.isEnabled()) {
+                    view.setEnabled(newValue);
+                }
+            }
+        });
+        boolean newValue = property.get() == null ? false : property.get();
+        if(newValue != view.isEnabled()) {
+            view.setEnabled(newValue);
+        }
+    }
+
+    @BindingAdapter({"app:checkedBinding"})
+    public static void bindCompoundButtonCheckedToBooleanProperty(final CompoundButton view, final Property<Boolean> property) {
+
+        view.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                property.set(b);
+            }
+        });
+
+
+        property.onChanged(new ValueChangeListener<Boolean>() {
+            @Override
+            public void valueChanged(ValueChangeEvent<? extends Boolean> evt) {
+                boolean newValue = evt.getNewValue() == null ? false : evt.getNewValue();
+                if(newValue != view.isChecked()) {
+                    view.setChecked(newValue);
+                }
+            }
+        });
+        boolean newValue = property.get() == null ? false : property.get();
+        if(newValue != view.isChecked()) {
+            view.setChecked(newValue);
+        }
+    }
+
+    @BindingAdapter({"app:textBinding"})
     public static void bindTextViewTextToStringProperty(final TextView view, final Property<String> property) {
         view.addTextChangedListener(new TextWatcher() {
             @Override
